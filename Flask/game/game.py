6c620e276a6cc6f1f44c4799e8964d1d66f0d628
wakeupmh <wakeupmh@gmail.com>
2019-12-24 15:29:43 -0300

    adding dynamic rendering and OOP

--- Flask/game/game.py
+++ Flask/game/game.py
@@ -2,9 +2,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+class Game:
+  def __init__(self, name, category, console):
+    self.name = name
+    self.category = category
+    self.console = console
+
 @app.route('/init')
 def hello():
-  list= ['Tetris', 'Super Mario', 'Pokemon Gold']
+  game1 = Game('Super Mario', 'adventure', 'SNES')
+  game2 = Game('Pokemon Gold', 'RPG', 'GBA')
+  list = [game1, game2]
   return render_template('list.html', props={'title': 'Games', 'games': list})
 
 app.run()
